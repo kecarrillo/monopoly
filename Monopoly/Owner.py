5862be43ec445986fd70eb7e11075110ee15cd98
greyr <kevin.carrillo@imie.fr>
2020-06-08 11:50:34 +0200

    Creation of the abstract class Owner

--- Monopoly/Owner.py
+++ Monopoly/Owner.py
@@ -1,4 +1,9 @@
-class Owner:
+from abc import ABCMeta, abstractmethod
+
+
+class Owner(metaclass=ABCMeta):
+
+    @abstractmethod
     def __init__(self, name, money):
         self.name = name
         self.money = money
